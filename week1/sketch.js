754dca90ea204e5ef4b335f08c260bca5827e334
Yuting Lin <157137776+YutingLin0@users.noreply.github.com>
2026-09-06 21:54:33 -0400

    Added quotes and sources for creative coding

--- week1/sketch.js
+++ week1/sketch.js
@@ -2,14 +2,14 @@
 // this is the only part of the file you need to edit!
 
 const quotes = [
-  { text: "the intersection of programming, design, and art", source: "Scott" },
-  { text: "a chance for code to be wrong", source: "Sara" },
-  { text: "surprise, exploration, and aesthetic exploration", source: "Sally" },
-  { text: "process over results", source: "Sam" },
-  { text: "form over function", source: "Sean" },
-  { text: "iterative and experimental", source: "Scooby" },
-  { text: "a community of practice", source: "Seth" },
-  { text: "an opportunity to stick it to the man", source: "Shifty-eyed Susan" }
+  { text: "the practice of making art with code", source: "Andrew Bryant" },
+  { text: "about experimentation, not functionality", source: "Maura Kelly" },
+  { text: "a process, based on exploration, iteration, reflection and discovery, where code is used as the primary medium to create a wide range of media artifacts", source: "Tim Rodenbröker" },
+  { text: "a practice where programming is used as a tool for art or design", source: "Jack B. Du" },
+  { text: "Creative coding is the intersection where artists and programmers meet, creating a vast degree of potential", source: "Meadows School of the Arts" },
+  { text: "the process of using programming languages to generate art and music, turning code into a paintbrush for real-time artistic expression", source: "University of York" },
+  { text: "an act of curiosity—an ongoing conversation with the unknown that begins with a simple question: what if?", source: "Patrik Hübner" },
+  { text: "the act of writing code for the pure pleasure of it, where the goal is not functional but creative and expressive; where code becomes a medium, just like a canvas for a painter", source: "Alessia" }
 ];
 // no need to edit anything below this line! 
 // if you have made an error, you can check your history to see what might have gone wrong
